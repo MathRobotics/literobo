f7cd6dadf7032d633c1b1d671c9845ec255864a8
Taiki Ishigaki <taiki000ishigaki@gmail.com>
2025-12-16 17:07:30 +0900

    Address PyO3 safety and numpy warnings

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(unsafe_op_in_unsafe_fn)]
+
 use nalgebra::{Isometry3, Matrix6xX, Rotation3, Translation3, Unit, UnitQuaternion, Vector3};
 use numpy::{PyArray2, ToPyArray};
 use pyo3::exceptions::PyValueError;
@@ -31,8 +33,11 @@ enum JointKind {
 
 #[derive(Clone, Debug)]
 struct ChainJoint {
+    #[allow(dead_code)]
     name: String,
+    #[allow(dead_code)]
     parent: String,
+    #[allow(dead_code)]
     child: String,
     origin: Isometry3<f64>,
     axis: Vector3<f64>,
@@ -304,6 +309,7 @@ struct PyRobot {
     inner: KinematicChain,
 }
 
+#[allow(unsafe_op_in_unsafe_fn)]
 #[pymethods]
 impl PyRobot {
     #[staticmethod]
@@ -329,15 +335,19 @@ impl PyRobot {
         &self,
         py: Python<'py>,
         joints: Vec<f64>,
-    ) -> PyResult<&'py PyArray2<f64>> {
+    ) -> PyResult<Bound<'py, PyArray2<f64>>> {
         let pose = self.inner.forward_kinematics(&joints)?;
         let matrix = pose.to_homogeneous();
-        Ok(matrix.to_pyarray(py))
+        Ok(matrix.to_pyarray_bound(py))
     }
 
-    fn jacobian<'py>(&self, py: Python<'py>, joints: Vec<f64>) -> PyResult<&'py PyArray2<f64>> {
+    fn jacobian<'py>(
+        &self,
+        py: Python<'py>,
+        joints: Vec<f64>,
+    ) -> PyResult<Bound<'py, PyArray2<f64>>> {
         let jac = self.inner.jacobian(&joints)?;
-        Ok(jac.to_pyarray(py))
+        Ok(jac.to_pyarray_bound(py))
     }
 }
 
@@ -352,7 +362,7 @@ fn py_from_urdf_str(urdf: &str, base_link: &str, end_link: &str) -> PyResult<PyR
 }
 
 #[pymodule]
-fn literobo(py: Python, m: &PyModule) -> PyResult<()> {
+fn literobo(py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_class::<PyRobot>()?;
     m.add("VERSION", env!("CARGO_PKG_VERSION"))?;
     m.add("BASE_LINK_KEY", "base_link")?;
